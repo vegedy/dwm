39034919c8d40493e30c37131c804d28a48422c7
bzenz <benito@benitozenz.de>
2026-09-16 13:26:39 +0200

    Patch: fsignal

--- config.def.h
+++ config.def.h
@@ -354,3 +354,11 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signum>"` */
+static Signal signals[] = {
+	/* signum       function        argument  */
+	{ 1,            xrdb,           {.v = NULL} },
+};
+
